742aef665b20670ddf0813e648168c5f91edd564
BotrikTorf <bodrikgame@gmail.com>
2022-09-09 13:02:56 +0300

    внес исправления после замечаний на первое задание

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -8,17 +8,17 @@ namespace TasksForJunior
         static void Main()
         {
             // First Task Variables
+            const ushort MinLife = 0;
+
             bool bulletHit = false;
             byte amountOfLife = byte.MaxValue;
             sbyte maxMana = sbyte.MaxValue;
             short minMana = 0;
-            const ushort minLife = 0;
             int manaAvailable = 100;
             uint thingsInBackpack;
             float cardLength;
-            char potionID;
+            char potionIndicator = 'H';
             string unitNames = "Drunk Tree";
-
         }
     }
 }
