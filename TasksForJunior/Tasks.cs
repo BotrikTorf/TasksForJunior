a91ef30548346eb602817807bacad48d79d7ad12
BotrikTorf <bodrikgame@gmail.com>
2022-09-09 15:57:40 +0300

    Почистил базовую ветку

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -19,16 +19,6 @@ namespace TasksForJunior
             float cardLength;
             char potionIndicator = 'H';
             string unitNames = "Drunk Tree";*/
-
-            sbyte userPictures = 52;
-            sbyte picturesInRow = 3;
-            sbyte numberOfRows;
-            numberOfRows = Convert.ToSByte(userPictures / picturesInRow);
-            sbyte cortinasBeyondMeasure = Convert.ToSByte(userPictures % picturesInRow);
-            Console.WriteLine("Чтобы повесить {0} картины по {1} в каждом ряду " +
-                "необходимо {2} рядов. Количество картин которые не поместились {3}" +
-                " для полного заполнения ряда",
-                userPictures, picturesInRow, numberOfRows, cortinasBeyondMeasure);
         }
     }
 }
