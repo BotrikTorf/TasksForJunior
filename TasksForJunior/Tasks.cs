41081de73c915151d9ed5b45eac0be1c83bdd271
BotrikTorf <bodrikgame@gmail.com>
2022-09-09 10:00:18 +0300

    Добавьте файлы проекта.

---
+++ TasksForJunior/Tasks.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace TasksForJunior
+{
+    class Tasks
+    {
+        static void Main()
+        {
+            // First Task Variables
+            bool bulletHit = false;
+            byte amountOfLife = byte.MaxValue;
+            sbyte maxMana = sbyte.MaxValue;
+            short minMana = 0;
+            const ushort minLife = 0;
+            int manaAvailable = 100;
+            uint thingsInBackpack;
+            float cardLength;
+            char potionID;
+            string unitNames = "Drunk Tree";
+
+        }
+    }
+}
