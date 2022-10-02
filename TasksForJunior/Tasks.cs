080b0ee176d223b530fe07a634d989b54b3b22fa
BotrikTorf <bodrikgame@gmail.com>
2022-10-02 16:07:10 +0300

    Обнуление

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -7,18 +7,7 @@ namespace TasksForJunior
     {
         static void Main()
         {
-            /*// First Task Variables
-            const ushort MinLife = 0;
 
-            bool bulletHit = false;
-            byte amountOfLife = byte.MaxValue;
-            sbyte maxMana = sbyte.MaxValue;
-            short minMana = 0;
-            int manaAvailable = 100;
-            uint thingsInBackpack;
-            float cardLength;
-            char potionIndicator = 'H';
-            string unitNames = "Drunk Tree";*/
         }
     }
 }
