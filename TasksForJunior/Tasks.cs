2bce2af350ee44dd8db33ec07dcb6fa77a9447b1
BotrikTorf <bodrikgame@gmail.com>
2022-10-03 09:37:25 +0300

    Решение задачи

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -7,7 +7,25 @@ namespace TasksForJunior
     {
         static void Main()
         {
+            bool isRight;
 
+            Console.Write("Введите число:");
+            isRight = int.TryParse(Console.ReadLine(), out int number);
+
+            while (!isRight)
+            {
+                Console.WriteLine("Вы набрали вместа числа стова!");
+                Console.Write("Введите число:");
+                string text = Console.ReadLine();
+                isRight = int.TryParse(text, out int numberRun);
+
+                if (isRight)
+                {
+                    number = numberRun;
+                }
+            }
+
+            Console.WriteLine($"Вы набрали число {number}");
         }
     }
 }
