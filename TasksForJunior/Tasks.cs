60f2c5abee35aae7deccaf4f9fede3b6a517745c
BotrikTorf <bodrikgame@gmail.com>
2022-10-04 14:22:21 +0300

    Все проще чем казалось.

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -6,28 +6,23 @@ namespace TasksForJunior
     {
         static void Main()
         {
-            Console.Write("Введите число: ");
-            string textNumber = Console.ReadLine();
-            Console.WriteLine($"Вы набрали число {ReadNumber(textNumber)}");
+            ReadNumber();
         }
 
-        private static int ReadNumber(string textNumber)
+        private static int ReadNumber()
         {
             int numberTemp = 0;
             bool isRead = true;
 
             while (isRead)
             {
+                Console.Write("Введите число: ");
+                string textNumber = Console.ReadLine();
+
                 if (int.TryParse(textNumber, out int number))
                 {
                     numberTemp = number;
                     isRead = false;
-                }
-                else
-                {
-                    Console.WriteLine("Вы ввели не правельно число!");
-                    Console.Write("Введите число: ");
-                    textNumber = Console.ReadLine();
                 }
             }
 
