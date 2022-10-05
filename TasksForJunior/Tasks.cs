2e658e1c7b61159bea0d5d57ea1653c3c85e22a5
BotrikTorf <bodrikgame@gmail.com>
2022-10-05 18:17:41 +0300

    Решение задачи

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -7,7 +7,58 @@ namespace TasksForJunior
     {
         static void Main()
         {
+            int[,] array = new int[10, 10];
 
+            array = FillsArray(array);
+            ShowArray(array);
+            Console.WriteLine();
+            array = Shuffle(array);
+            ShowArray(array);
+        }
+
+        private static int[,] Shuffle(int[,] array)
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    int iTemp = random.Next(0, array.GetLength(0));
+                    int jTemp = random.Next(0, array.GetLength(1));
+                    int tempNumber = array[iTemp, jTemp];
+                    array[iTemp, jTemp] = array[i, j];
+                    array[i, j] = tempNumber;
+                }
+            }
+
+            return array;
+        }
+
+        private static void ShowArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write($"{array[i, j]}\t");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static int[,] FillsArray(int[,] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = (i + 1) * (j + 1);
+                }
+            }
+
+            return array;
         }
     }
 }
