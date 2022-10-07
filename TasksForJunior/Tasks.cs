45c879329775920f018773fb4f24d1961269585e
BotrikTorf <bodrikgame@gmail.com>
2022-10-07 10:41:30 +0300

    Переделал код.

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 
 namespace TasksForJunior
@@ -8,17 +9,18 @@ namespace TasksForJunior
         static void Main()
         {
             bool isPlaying = true;
-            int mobPositionX = 37; 
+            int mobPositionX = 37;
             int mobPositionY = 2;
-            int mobDX = 0;
-            int mobDY = 0;
+            int mobDirectioX = 0;
+            int mobDirectioY = 0;
             int food = 0;
+            int foodMax;
             string typeCell = " ";
 
             string[] map =
                 { "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%",
-                  "%    %%     %%               %%        %",
-                  "%    %%  $  %%        %%     %%        %",
+                  "%    ||     %%               %%        %",
+                  "%    ||  $  %%        %%     %%        %",
                   "%    %%     %%    $   %%     %%        %",
                   "%    %%     %%        %%     %%        %",
                   "%    %%     %%        %%     %%   $    %",
@@ -27,10 +29,11 @@ namespace TasksForJunior
                   "%    %%     %%        %%     %%        %",
                   "%    %%  $  %%        %%     %%        %",
                   "%    %%               %%               %",
-                  "%    %%               %%               %",
+                  "% #  %%               %%               %",
                   "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%" };
 
             string[,] mapGame = ConvertsArrayOneToTwoDimensional(map);
+            foodMax = CountsFood(mapGame);
             DrawMap(mapGame);
             Console.CursorVisible = false;
 
@@ -40,84 +43,81 @@ namespace TasksForJunior
                 Console.Write("@");
                 ConsoleKeyInfo key = Console.ReadKey(true);
 
-                ControlsCharacter(ref mobDX, ref mobDY, key);
-                MovesMob(ref mobPositionX, ref mobPositionY, ref typeCell, mobDX, mobDY, mapGame);
-                CheckedFood(ref food, typeCell, mobPositionX, mobPositionY, mapGame);
-                ShowWinGame(ref mobPositionX, ref mobPositionY, typeCell);
+                ControlsCharacter(ref mobDirectioX, ref mobDirectioY, key);
+                MovesMob(ref mobPositionX, ref mobPositionY, ref typeCell, mobDirectioX, mobDirectioY, mapGame);
 
-                if (mobPositionX == 0 && mobPositionY == 0)
+                if (typeCell == "$")
+                {
+                    food++;
+                }
+
+                if (food == foodMax)
+                {
+                    ChangesMap(mapGame);
+                    Console.Clear();
+                    DrawMap(mapGame);
+                    Console.SetCursorPosition(mobPositionX, mobPositionY);
+                    Console.Write("@");
+                    food++;
+                }
+
+                if (typeCell == "#")
                 {
                     isPlaying = false;
+                    ShowWinGame();
                 }
             }
 
             Console.ReadLine();
         }
 
-        private static void CheckedFood(ref int food, string typeCell, int X, int Y, string[,] map)
+        private static void ChangesMap(string[,] map)
         {
-            if (typeCell == "$")
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                food++;
-            }
-
-            if (food == 5)
-            {
-                map[1, 5] = " ";
-                map[1, 6] = " ";
-                map[11, 2] = "#";
-
-                for (int i = 0; i < map.GetLength(0); i++)
+                for (int j = 0; j < map.GetLength(1); j++)
                 {
-                    for (int j = 0; j < map.GetLength(1); j++)
+                    if (map[i, j] == "|" || map[i, j] == "$")
                     {
-                        if (map[i, j] == "$")
-                        {
-                            map[i, j] = " ";
-                        }
+                        map[i, j] = " ";
                     }
                 }
-
-                Console.SetCursorPosition(0, 0);
-                DrawMap(map);
-                Console.SetCursorPosition(X, Y);
-                Console.Write("@");
             }
         }
 
-        private static void ControlsCharacter(ref int mobDX, ref int mobDY, ConsoleKeyInfo key)
+        private static void ControlsCharacter(ref int mobDirectioX, ref int mobDirectioY, ConsoleKeyInfo key)
         {
             switch (key.Key)
             {
                 case ConsoleKey.UpArrow:
-                    mobDX = 0;
-                    mobDY = -1;
+                    mobDirectioX = 0;
+                    mobDirectioY = -1;
                     break;
                 case ConsoleKey.DownArrow:
-                    mobDX = 0;
-                    mobDY = 1;
+                    mobDirectioX = 0;
+                    mobDirectioY = 1;
                     break;
                 case ConsoleKey.LeftArrow:
-                    mobDX = -1;
-                    mobDY = 0;
+                    mobDirectioX = -1;
+                    mobDirectioY = 0;
                     break;
                 case ConsoleKey.RightArrow:
-                    mobDX = 1;
-                    mobDY = 0;
+                    mobDirectioX = 1;
+                    mobDirectioY = 0;
                     break;
             }
         }
 
-        private static void MovesMob(ref int X, ref int Y,ref string typeCell, int DX, int DY, string[,] map)
+        private static void MovesMob(ref int mobPositionX, ref int mobPositionY, ref string typeCell, int mobDirectioX, int mobDirectioY, string[,] map)
         {
-            if (map[Y + DY, X + DX] == " " || map[Y + DY, X + DX] == "$" || map[Y + DY, X + DX] == "#")
+            if (map[mobPositionY + mobDirectioY, mobPositionX + mobDirectioX] != "%")
             {
-                Console.SetCursorPosition(X, Y);
+                Console.SetCursorPosition(mobPositionX, mobPositionY);
                 Console.Write(" ");
-                X += DX;
-                Y += DY;
-                typeCell = map[Y, X];
-                Console.SetCursorPosition(X, Y);
+                mobPositionX += mobDirectioX;
+                mobPositionY += mobDirectioY;
+                typeCell = map[mobPositionY, mobPositionX];
+                Console.SetCursorPosition(mobPositionX, mobPositionY);
                 Console.Write("@");
             }
         }
@@ -141,26 +141,35 @@ namespace TasksForJunior
 
             for (int i = 0; i < array.Length; i++)
             {
-                string temp = array[i];
-
                 for (int j = 0; j < array[i].Length; j++)
                 {
-                    arrayTemp[i, j] = temp[j].ToString();
+                    arrayTemp[i, j] = array[i][j].ToString();
                 }
             }
 
             return arrayTemp;
         }
 
-        static void ShowWinGame(ref int mobX, ref int mobY, string typeCell)
+        static void ShowWinGame()
         {
-            if (typeCell == "#")
+            Console.Clear();
+            Console.WriteLine("Вы прошли лабиринт!");
+        }
+
+        static int CountsFood(string[,] map)
+        {
+            int food = 0;
+
+            for (int i = 0; i < map.GetLength(0); i++)
             {
-                mobX = 0;
-                mobY = 0;
-                Console.Clear();
-                Console.WriteLine("Вы прошли лабиринт!");
+                for (int j = 0; j < map.GetLength(1); j++)
+                {
+                    if (map[i,j] == "$")
+                        food++;
+                }
             }
+
+            return food;
         }
     }
 }
