f753fb77b89fb12d2ddef6aee94bd36e0a19ecda
BotrikTorf <bodrikgame@gmail.com>
2022-10-10 10:28:58 +0300

    the solution of the problem

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace TasksForJunior
 {
@@ -7,7 +7,80 @@ namespace TasksForJunior
     {
         static void Main()
         {
+            const string CommandAddDossier = "add";
+            const string CommandShowAllDossiers = "all";
+            const string CommandDeleteDossier = "delete";
+            const string CommandExit = "exit";
+            bool isWork = true;
 
+            Dictionary<string, string> dossiers = new Dictionary<string, string>();
+
+            Console.WriteLine("Добро пожаловать в программу кадровый учет.");
+            Console.WriteLine($"Для добавления досье введите команду {CommandAddDossier},\n" +
+                $"Для просмотра полного списка всех рабртающих людей введите команду {CommandShowAllDossiers},\n" +
+                $"Для удаления досье введите команду {CommandDeleteDossier},\n" +
+                $"Для выхода из программы введите команду {CommandExit}.");
+
+            while (isWork)
+            {
+                Console.Write("Введите команду: ");
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case CommandAddDossier:
+                        AddDossier(dossiers);
+                        break;
+                    case CommandShowAllDossiers:
+                        ShowAllDossiers(dossiers);
+                        break;
+                    case CommandDeleteDossier:
+                        DeletesDossier(dossiers);
+                        break;
+                    case CommandExit:
+                        isWork = false;
+                        break;
+                    default:
+                        Console.WriteLine("Вы не правильно ввели команду! Попробуйте еще раз.");
+                        break;
+                }
+            }
+        }
+
+        private static void DeletesDossier(Dictionary<string, string> dossiers)
+        {
+            Console.Write("Введите ФИО работника которого вы хотите удалить: ");
+            string fullName = Console.ReadLine();
+
+            if (dossiers.ContainsKey(fullName))
+            {
+                dossiers.Remove(fullName);
+                Console.WriteLine("Удаление прошло успешно.");
+            }
+            else
+            {
+                Console.WriteLine("Сотрудника с таким ФИО нет.");
+            }
+        }
+
+        private static void ShowAllDossiers(Dictionary<string, string> dossiers)
+        {
+            int number = 1;
+
+            foreach (var person in dossiers)
+            {
+                Console.WriteLine($"{number}. {person.Key} - {person.Value}.");
+                number++;
+            }
+        }
+
+        static void AddDossier(Dictionary<string, string> dossiers)
+        {
+            Console.Write("Введите ФИО работника: ");
+            string fullName = Console.ReadLine();
+            Console.Write("Введите профессию работника: ");
+            string profession = Console.ReadLine();
+            dossiers.Add(fullName, profession);
         }
     }
 }
