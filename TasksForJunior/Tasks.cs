706619f567f398e7313d0f0666fad21833f86423
BotrikTorf <bodrikgame@gmail.com>
2022-10-10 05:37:11 +0300

    Решение задачи

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections.Generic;
 
 namespace TasksForJunior
 {
@@ -7,7 +7,54 @@ namespace TasksForJunior
     {
         static void Main()
         {
+            const string CommandSum = "sum";
+            const string CommandExit = "exit";
+            bool isWork = true;
+            List<int> array = new List<int>();
 
+            Console.WriteLine($"Для добавления числа в массив введите число.\n" +
+                $"Для вычисления ссумы всех элементов массива введите команду {CommandSum}.\n" +
+                $"Для выхода из программы введите команду {CommandExit}.");
+
+            while (isWork)
+            {
+                string inputString = Console.ReadLine().ToLower();
+
+                if (int.TryParse(inputString, out int number))
+                {
+                    array.Add(number);
+                }
+                else
+                {
+                    if (inputString == CommandExit || inputString == CommandSum)
+                    {
+                        if (inputString == CommandExit)
+                        {
+                            isWork = false;
+                        }
+                        else
+                        {
+                            SumArray(array);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Вы не правельно ввели команду");
+                    }
+                }
+            }
+        }
+
+        static void SumArray(List<int> array)
+        {
+            int sum = 0;
+
+            foreach (var number in array)
+            {
+                sum += number;
+            }
+
+            Console.WriteLine($"Сумма всех элементов массива равна {sum}");
         }
     }
 }
