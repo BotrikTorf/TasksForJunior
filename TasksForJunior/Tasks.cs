3604a039f1e81fb9b47972d280c89d931952e795
BotrikTorf <bodrikgame@gmail.com>
2022-11-02 22:19:18 +0300

    Reworked code

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace TasksForJunior
 {
@@ -8,294 +7,226 @@ namespace TasksForJunior
     {
         static void Main()
         {
-            Salesman salesman = new Salesman();
-            Player player = new Player();
+            Player player = new Player(10);
+            Seller seller = new Seller(-1);
+            Shop shop = new Shop(player, seller);
+            shop.Trade();
+        }
+    }
+    enum ProductNamePrice
+    {
+        sword = 7,
+        axe = 8,
+        hammer = 9,
+        apple = 1,
+        pear = 2,
+        bread = 3,
+        water = 4,
+        milk = 5,
+        tea = 6
+    }
 
-            Bargaining bargaining = new Bargaining(player, salesman);
-            bargaining.StartTrading();
-            salesman.Show();
-            player.Show();
+    abstract class Peolple
+    {
+        private int _money;
+
+        public Peolple(int money) => _money = money;
+
+        protected int Money { get { return _money; } private set { } }
+
+        protected int GiveMoney(int requestedAmount)
+        {
+            if (requestedAmount < _money)
+            {
+                _money -= requestedAmount;
+                return requestedAmount;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        protected void TakeMoney(int money) => _money += money;
+    }
+
+    class Player : Peolple
+    {
+        private List<Product> _products;
+
+        public Player(int money = 0) : base(money) => _products = new List<Product>();
+
+        public int Money { get { return base.Money; } private set { } }
+
+        public void TakeProduct(Product product) => _products.Add(product);
+
+        public new void TakeMoney(int money) => base.TakeMoney(money);
+
+        public new int GiveMoney(int requestedAmount) => base.GiveMoney(requestedAmount);
+        
+        public Product GiveProduct(string name)
+        {
+            Product temProduct = new Product();
+
+            bool haveProduct = false;
+
+            foreach (var product in _products)
+            {
+                if (product.Name == name)
+                {
+                    temProduct = product;
+                    haveProduct = true;
+                    break;
+                }
+            }
+
+            if (haveProduct)
+            {
+                _products.Remove(temProduct);
+                return temProduct;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        public void ShowThings()
+        {
+            if (_products.Count > 0)
+                foreach (var product in _products)
+                    Console.WriteLine($"Наименование товара: {product.Name}, цена товара: {product.Cost}");
+            else
+                Console.WriteLine("Вещей нет.");
         }
     }
 
-    abstract class Subject
+    class Seller : Peolple
     {
-        private readonly int _price;
-        private readonly string _title;
+        public Seller(int money = 0) : base(money) { }
 
-        public Subject(int price, string title)
+        public int Money { get { return base.Money; } private set { } }
+
+        public new void TakeMoney(int money) => base.TakeMoney(money);
+
+        public new int GiveMoney(int requestedAmount) => base.GiveMoney(requestedAmount);
+    }
+
+    class Product
+    {
+        private string _name;
+        private int _cost;
+        public Product(string name = null, int cost = 0)
         {
-            _price = price;
-            _title = title;
+            _name = name;
+            _cost = cost;
         }
 
-        public void Show()
+        public string Name { get { return _name; } private set { } }
+        public int Cost { get { return _cost; } private set { } }
+    }
+
+    class Cell
+    {
+        private List<Product> _products = new List<Product>();
+        public Cell()
         {
-            Console.WriteLine($"Название: {_title}\n" +
-                              $"Стоимость: {_price}");
+            foreach (var product in Enum.GetValues(typeof(ProductNamePrice)))
+                _products.Add(new Product(product.ToString(), (int)product));
+        }
+
+        public Product GivProduct(string name)
+        {
+            Product temProduct = new Product();
+
+            bool haveProduct = false;
+
+            foreach (var product in _products)
+            {
+                if (product.Name == name)
+                {
+                    temProduct = product;
+                    haveProduct = true;
+                    break;
+                }
+            }
+
+            if (haveProduct)
+            {
+                _products.Remove(temProduct);
+                return temProduct;
+            }
+            else
+            {
+                return null;
+            }
+        }
+        
+        public void TakeProduct(Product product) => _products.Add(product);
+
+        public void ShowThings()
+        {
+            if (_products.Count > 0)
+                foreach (var product in _products)
+                    Console.WriteLine($"Наименование товара: {product.Name}, цена товара: {product.Cost}");
+            else
+                Console.WriteLine("В магазине нет това. Все скупили.");
         }
     }
 
-    class FoodManna : Subject
+    class Shop
     {
-        private readonly int _amountManna;
+        private Player _player = new Player();
+        private Seller _seller = new Seller();
+        private Cell _cell = new Cell();
 
-        public FoodManna(int amountManna, int price, string title) : base(price, title)
-        {
-            _amountManna = amountManna;
-        }
-
-        public new void Show()
-        {
-            base.Show();
-            Console.WriteLine($"Восполняет манну на {_amountManna} единиц.");
-        }
-    }
-
-    class FoodHP : Subject
-    {
-        private readonly int _amountHP;
-
-        public FoodHP(int amountHP, int price, string title) : base(price, title)
-        {
-            _amountHP = amountHP;
-        }
-
-        public new void Show()
-        {
-            base.Show();
-            Console.WriteLine($"Восполняет очки жизни на {_amountHP} единиц.");
-        }
-    }
-
-    class Weapon : Subject
-    {
-        private readonly int _maxDamage;
-        private readonly int _minDamage;
-
-        public Weapon(int maxDamage, int minDamage, int price, string title) : base(price, title)
-        {
-            _minDamage = minDamage;
-            _maxDamage = maxDamage;
-        }
-
-        public new void Show()
-        {
-            base.Show();
-            Console.WriteLine($"Урон: {_minDamage} - {_maxDamage} единиц.");
-        }
-    }
-
-    class Player
-    {
-        private readonly List<Weapon> _weapons;
-        private readonly List<FoodManna> _foodsManna;
-        private readonly List<FoodHP> _foodsHP;
-
-        public Player()
-        {
-            _weapons = new List<Weapon>(10);
-            _foodsHP = new List<FoodHP>(50);
-            _foodsManna = new List<FoodManna>(50);
-        }
-
-        public void Purchase(Subject subject)
-        {
-            if (subject is Weapon weapon)
-            {
-                _weapons.Add(weapon);
-            }
-            else if (subject is FoodHP foodHP)
-            {
-                _foodsHP.Add(foodHP);
-            }
-            else if (subject is FoodManna foodManna)
-            {
-                _foodsManna.Add(foodManna);
-            }
-        }
-
-        public void Show()
-        {
-            Console.WriteLine("У игрока имеется в наличии:");
-            Console.WriteLine("Оружие: ");
-
-            foreach (var weapon in _weapons)
-            {
-                weapon.Show();
-            }
-
-            Console.WriteLine("Еда восполняющая очки жизни: ");
-
-            foreach (var food in _foodsHP)
-            {
-                food.Show();
-            }
-
-            Console.WriteLine("Еда восполняющая очки манны: ");
-
-            foreach (var food in _foodsManna)
-            {
-                food.Show();
-            }
-        }
-    }
-
-    class Salesman
-    {
-        private const int PriceOneWeapon = 10;
-        private const int PriceTwoWeapon = 20;
-        private const int PriceFood = 3;
-        private const int OneHandedWeaponDamageRatio = 1;
-        private const int TowHandedWeaponDamageRatio = 10;
-
-
-        private readonly string[] oneHandedWeapon = { "OneHandedSword", "OneHandedAxe", "OneHandedHammer" };
-        private readonly string[] twoHandedWeapon = { "TwoHandedSword", "TwoHandedAxe", "TwoHandedHammer" };
-        private readonly string[] foodsNameHP = { "Apple", "Pear", "Bread" };
-        private readonly string[] foodsNameManna = { "Water", "Milk", "Tea" };
-
-        private readonly List<Weapon> _weapons;
-        private readonly List<FoodManna> _foodsManna;
-        private readonly List<FoodHP> _foodsHP;
-        private readonly Random _random = new Random();
-
-        public Salesman()
-        {
-            _weapons = new List<Weapon>();
-
-            CreatesWeapon(oneHandedWeapon, PriceOneWeapon, OneHandedWeaponDamageRatio);
-            CreatesWeapon(twoHandedWeapon, PriceTwoWeapon, TowHandedWeaponDamageRatio);
-
-            _foodsHP = new List<FoodHP>();
-
-            for (int i = 0; i < foodsNameHP.Length; i++)
-            {
-                _foodsHP.Add(new FoodHP(i * 10, PriceFood, foodsNameHP[i]));
-            }
-
-            _foodsManna = new List<FoodManna>();
-
-            for (int i = 0; i < foodsNameManna.Length; i++)
-            {
-                _foodsManna.Add(new FoodManna(i * 10, PriceFood, foodsNameManna[i]));
-            }
-        }
-
-        public List<Weapon> ListWeapons()
-        {
-            return _weapons;
-        }
-
-        public List<FoodHP> ListFoodHps()
-        {
-            return _foodsHP;
-        }
-
-        public List<FoodManna> ListFoodManna()
-        {
-            return _foodsManna;
-        }
-
-        public void Sale(Subject subject)
-        {
-            if (subject is Weapon weapon)
-            {
-                _weapons.Remove(weapon);
-            }
-            else if (subject is FoodHP foodHP)
-            {
-                _foodsHP.Remove(foodHP);
-            }
-            else if (subject is FoodManna foodManna)
-            {
-                _foodsManna.Remove(foodManna);
-            }
-        }
-
-        public void Show()
-        {
-            Console.WriteLine("У продовца есть в наличии: ");
-            Console.WriteLine("Оружие: ");
-
-            foreach (var weapon in _weapons)
-            {
-                weapon.Show();
-            }
-
-            Console.WriteLine("Еда восполняющая очки жизни: ");
-
-            foreach (var food in _foodsHP)
-            {
-                food.Show();
-            }
-
-            Console.WriteLine("Еда восполняющая очки манны: ");
-
-            foreach (var food in _foodsManna)
-            {
-                food.Show();
-            }
-        }
-
-        private void CreatesWeapon(string[] name, int PriceOneWeapon, int damage)
-        {
-            for (int i = 0; i < name.Length; i++)
-            {
-                int lowerMinDamage = 1 * damage;
-                int upperMinDamage = 3 * damage;
-                int lowerMaxDamage = 4 * damage;
-                int upperMaxDamage = 6 * damage;
-                int minDamage = _random.Next(lowerMinDamage, upperMinDamage);
-                int maxDamage = _random.Next(lowerMaxDamage, upperMaxDamage);
-                _weapons.Add(new Weapon(maxDamage, minDamage, PriceOneWeapon, name[i]));
-            }
-        }
-    }
-
-    class Bargaining
-    {
-        private const string CommandWeapon = "weapon";
-        private const string CommandFoodManna = "manna";
-        private const string CommandFoodHP = "hp";
-        private const string CommandExit = "exit";
-
-        private readonly Player _player;
-        private readonly Salesman _salesman;
-
-        private bool isTrading = true;
-
-        public Bargaining(Player player, Salesman salesman)
+        public Shop(Player player, Seller seller)
         {
             _player = player;
-            _salesman = salesman;
+            _seller = seller;
         }
 
-        public void StartTrading()
+        public void Trade()
         {
-            Console.WriteLine("Добро пожаловать в наш маленький магазинчик!\n" +
-                              $"Если вы хотите посмотреть оружие наберите {CommandWeapon}.\n" +
-                              $"Если хотите посмотреть предметы для востановления вашей жизни наберите - {CommandFoodHP}.\n" +
-                              $"Если хотите посмотреть предметы для востановления вашей манны наберите - {CommandFoodManna}.\n" +
-                              $"Для прекращения торговли наберите - {CommandExit}.");
+            const string CommandExit = "exit";
+            const string CommandShowProduct = "show";
+            const string CommandBuy = "buy";
+            const string CommandSell = "sell";
+            const string CommandViewBalance = "balance";
+            const string CommandShowThings = "player";
 
-            while (isTrading)
+            bool isTrade = true;
+
+            Console.WriteLine("Добро пожаловать в наш маленький магазинчик!\n" +
+                              $"{CommandShowProduct} - посмотреть товар.\n" +
+                              $"{CommandBuy} - покупка товара.\n" +
+                              $"{CommandSell} - продажа товара.\n" +
+                              $"{CommandExit} - прекращениe торговли.\n" +
+                              $"{CommandViewBalance} - просмотр баланса у продовца и игрока.\n" +
+                              $"{CommandShowThings} - показать вещи игрока.");
+
+            while (isTrade)
             {
-                Console.Write("Введите команду для просмотра и покупки нужного вам товара: ");
+                Console.Write("Введите команду: ");
 
                 switch (Console.ReadLine())
                 {
                     case CommandExit:
-                        isTrading = false;
+                        isTrade = false;
                         break;
-                    case CommandFoodHP:
-                        TradinHP(_salesman.ListFoodHps());
+                    case CommandShowProduct:
+                        _cell.ShowThings();
                         break;
-                    case CommandFoodManna:
-                        TradinManna(_salesman.ListFoodManna());
+                    case CommandBuy:
+                        Buy();
                         break;
-                    case CommandWeapon:
-                        TradinWeapon(_salesman.ListWeapons());
+                    case CommandSell:
+                        Sell();
+                        break;
+                    case CommandViewBalance:
+                        ViewBalance();
+                        break;
+                    case CommandShowThings:
+                        _player.ShowThings();
                         break;
                     default:
                         Console.WriteLine("Вы не правельно ввели команду! Попробуйте еще раз");
@@ -304,53 +235,50 @@ namespace TasksForJunior
             }
         }
 
-        private void TradinHP(List<FoodHP> foodsHp)
+        private void ViewBalance() => Console.WriteLine($"Баланс денег у игрока: {_player.Money}.\n" +
+                                                        $"Баланс денег у продовца: {_seller.Money}.");
+
+        private void Sell()
         {
-            List<Subject> subject = foodsHp.Cast<Subject>().ToList();
+            Console.Write("Ведите название товара: ");
 
-            Tradin(subject);
-        }
+            Product product = _player.GiveProduct(Console.ReadLine());
 
-        private void TradinManna(List<FoodManna> foodsManna)
-        {
-            List<Subject> subject = foodsManna.Cast<Subject>().ToList();
-
-            Tradin(subject);
-        }
-
-        private void TradinWeapon(List<Weapon> weapons)
-        {
-            List<Subject> subject = weapons.Cast<Subject>().ToList();
-
-            Tradin(subject);
-        }
-
-        private void Tradin(List<Subject> subjects)
-        {
-            int serialNumber = 0;
-
-            Console.WriteLine("Представляю вам список товара: ");
-
-            foreach (var product in subjects)
+            if (product == null)
             {
-                Console.WriteLine($"Порядковый номер: {serialNumber}");
-                product.Show();
-                serialNumber++;
+                Console.WriteLine("Такого товара нет! Вы ввели неправельно нозвание товара или у игрока такого товара нет.");
             }
-
-            Console.Write("Для покупки товара введите порядковый номер: ");
-
-            if (int.TryParse(Console.ReadLine(), out int number))
+            else if (_seller.GiveMoney(product.Cost) == 0)
             {
-                if (number < subjects.Count)
-                {
-                    _player.Purchase(subjects[number]);
-                    _salesman.Sale(subjects[number]);
-                }
-                else
-                {
-                    Console.WriteLine("Товара с таким номером нет.");
-                }
+                _player.TakeProduct(product);
+                Console.WriteLine("У продовца не хватает денег.");
+            }
+            else
+            {
+                _cell.TakeProduct(product);
+                _player.TakeMoney(product.Cost);
+            }
+        }
+
+        private void Buy()
+        {
+            Console.Write("Ведите название товара: ");
+
+            Product product = _cell.GivProduct(Console.ReadLine());
+
+            if (product == null)
+            {
+                Console.WriteLine("Такого товара нет! В магазине скупили весь товар или вы ввели неправильно название товара.");
+            }
+            else if (_player.GiveMoney(product.Cost) == 0)
+            {
+                _cell.TakeProduct(product);
+                Console.WriteLine("У игрока не хватает денег.");
+            }
+            else
+            {
+                _player.TakeProduct(product);
+                _seller.TakeMoney(product.Cost);
             }
         }
     }
