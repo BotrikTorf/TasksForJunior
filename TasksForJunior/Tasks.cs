87442155032abea41b76dd00741a3decdc8c6f0c
bodrik_torf <bodrik_torf@mail.ru>
2022-12-13 17:03:05 +0300

    Start

--- TasksForJunior/Tasks.cs
+++ TasksForJunior/Tasks.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TasksForJunior
 {
@@ -7,18 +8,7 @@ namespace TasksForJunior
     {
         static void Main()
         {
-            /*// First Task Variables
-            const ushort MinLife = 0;
 
-            bool bulletHit = false;
-            byte amountOfLife = byte.MaxValue;
-            sbyte maxMana = sbyte.MaxValue;
-            short minMana = 0;
-            int manaAvailable = 100;
-            uint thingsInBackpack;
-            float cardLength;
-            char potionIndicator = 'H';
-            string unitNames = "Drunk Tree";*/
         }
     }
 }
